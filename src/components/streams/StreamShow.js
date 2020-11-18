015e0b8e6d5f2855d0cab9126b39aafff6a5c2a1
Vlad Blank <blankvladp@gmail.com>
2020-11-18 19:35:36 +0200

    add stream functionality with obs

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
+
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+
+    this.buildPlayer();
+  };
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+
+    if(this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -15,6 +50,7 @@ class StreamShow extends React.Component {
     } else {
       return (
         <div>
+          <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
           <h3>{this.props.stream.title}</h3>
           <p>{this.props.stream.description}</p>
         </div>
